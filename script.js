bf0c9c911b7d7391831cf207cb48ecbe66cc9f30
BryanBarrozo <bryanbarrozox2@gmail.com>
2026-05-20 04:10:46 -0300

    3 commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//688c0777584fd7164ff2dfe24c5ad896
 //Variables
 const api_key = "688c0777584fd7164ff2dfe24c5ad896";
 
@@ -23,12 +22,20 @@ const visibility = document.querySelector("#visibility");
 const pressure = document.querySelector("#pressure");
 const sea_level = document.querySelector("#sea_level");
 
+const weather_icon = document.querySelector("#weather_icon");
+
 //Variables forecast cards
 const forecast_1 = document.querySelector("#forecast_1");
 const forecast_2 = document.querySelector("#forecast_2");
 const forecast_3 = document.querySelector("#forecast_3");
 const forecast_4 = document.querySelector("#forecast_4");
 const forecast_5 = document.querySelector("#forecast_5");
+
+const forecast_1_img = document.querySelector("#forecast_1_img")
+const forecast_2_img = document.querySelector("#forecast_2_img")
+const forecast_3_img = document.querySelector("#forecast_3_img")
+const forecast_4_img = document.querySelector("#forecast_4_img")
+const forecast_5_img = document.querySelector("#forecast_5_img")
 
 const forecast_1_temp = document.querySelector("#forecast_1_temp");
 const forecast_2_temp = document.querySelector("#forecast_2_temp");
@@ -39,7 +46,6 @@ const forecast_5_temp = document.querySelector("#forecast_5_temp");
 
 climate_search(input_city.value = "london");
 
-
 // Functions
 function change_html(data){
 
@@ -48,38 +54,43 @@ function change_html(data){
     high_temp.innerHTML = `Max: ${Math.floor(data.main.temp_max)}°C`;
     lower_temp.innerHTML = `Min: ${Math.floor(data.main.temp_min)}°C`;
     climate.innerHTML = data.weather[0].description;
+    weather_icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
     //cards
     feels.innerHTML =`Feels Like: ${Math.round(data.main.feels_like)}°C`;
     humidity.innerHTML =`${data.main.humidity}%`;
     wind.innerHTML =`${data.wind.speed}km/h`
     visibility.innerHTML = `${data.visibility/1000} km`;
-    pressure.innerHTML =`${data.main.pressure}hPa`
-    sea_level.innerHTML =`${data.main.sea_level}`
+    pressure.innerHTML =`${data.main.pressure} hPa`
+    sea_level.innerHTML =`${data.main.sea_level} hPa`
+
+    
 
 }
 
 function change_forecast(data){
-
-    forecast_1.innerHTML = data.list[0].dt_txt.split(" ")[1].slice(0,5);//separando data da hora
+    forecast_1.innerHTML = data.list[0].dt_txt.split(" ")[1].slice(0,5);
+    forecast_1_img.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
     forecast_1_temp.innerHTML = (`${Math.floor(data.list[0].main.temp)}°C`)
 
     forecast_2.innerHTML = data.list[1].dt_txt.split(" ")[1].slice(0,5);
+    forecast_2_img.src = `https://openweathermap.org/img/wn/${data.list[1].weather[0].icon}@2x.png`;
     forecast_2_temp.innerHTML = (`${Math.floor(data.list[1].main.temp)}°C`)
 
     forecast_3.innerHTML = data.list[2].dt_txt.split(" ")[1].slice(0,5);
+    forecast_3_img.src = `https://openweathermap.org/img/wn/${data.list[2].weather[0].icon}@2x.png`;
     forecast_3_temp.innerHTML = (`${Math.floor(data.list[2].main.temp)}°C`)
 
     forecast_4.innerHTML = data.list[3].dt_txt.split(" ")[1].slice(0,5);
+    forecast_4_img.src = `https://openweathermap.org/img/wn/${data.list[3].weather[0].icon}@2x.png`;
     forecast_4_temp.innerHTML = (`${Math.floor(data.list[3].main.temp)}°C`)
 
     forecast_5.innerHTML = data.list[4].dt_txt.split(" ")[1].slice(0,5);
+    forecast_5_img.src = `https://openweathermap.org/img/wn/${data.list[4].weather[0].icon}@2x.png`;
     forecast_5_temp.innerHTML = (`${Math.floor(data.list[4].main.temp)}°C`)
-
 }
 
 async function climate_search() {
-
     const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${input_city.value}&appid=${api_key}&units=metric`
     );
@@ -90,30 +101,22 @@ async function climate_search() {
 
     const forecast_data = await response_forecast.json();
 
+    
 
     change_html(data);
     change_forecast(forecast_data);
 
-      
 }
 
 // Events
 
-input_city.addEventListener("change", () => {//mudar para input dinamico
-    climate_search();
+input_city.addEventListener("keydown", (event) => {
+    x = event.key
+    if(x === "Enter"){
+        climate_search();
+    }
 });
 
-/*
-input_city.addEventListener("change", () =>{
-    async function input_search(){
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input_city.value}&limit=5&appid=${api_key}`)
-
-        const list = await response.json()
-        console.log(list)
-    }  
-    input_search();
-    })
-*/
 
 
 /*
